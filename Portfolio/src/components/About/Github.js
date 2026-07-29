2c908b09ec8b63dbf056e9fc13f030e9b977c377
Olaseigbe Raphael <raphaeldiego79@gmail.com>
2026-07-30 00:37:27 +0100

    about fix

--- Portfolio/src/components/About/Github.js
+++ Portfolio/src/components/About/Github.js
@@ -14,12 +14,13 @@ function Github() {
       <h1 className="project-heading pb-4" style={{ paddingBottom: "20px" }}>
         Days I <strong className="lime">Code</strong>
       </h1>
+      
       <GitHubCalendar
-        username="soumyajit4419"
-        blockSize={30}
-        blockMargin={10}
+        username="Raph3rr"
+        blockSize={15}
+        blockMargin={5}
         color="#d7ff00"
-        fontSize={20}
+        fontSize={16}
       />
     </Row>
   );
